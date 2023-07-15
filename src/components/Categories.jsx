0fd3d89000515d4999c7f8b8fc881e7cf5d7e4d7
Dipesh Shivrame <dipeshss@Dipeshs-MacBook-Air.local>
2023-07-15 13:22:10 +0530

    navigate after filtering

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,28 +2,36 @@ import headphone from '../assets/images/wireless-02.png';
 import earbuds from '../assets/images/earbuds.webp';
 import smwatch from '../assets/images/smartwatch.png';
 import { useProductsContext } from '../contexts';
+import { useNavigate } from 'react-router-dom';
 
 function Categories() {
     const { applyFilter } = useProductsContext(); 
+    const navigate = useNavigate();
+
+    function filterAndNavigate(type) {
+        applyFilter('categories', type);
+        navigate('/products');
+    }
+    
     return (
         <div className="mt-8 text-center">
             <h3 className="text-4xl font-semibold my-2">Categories</h3>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 { }
                 <div className="flex flex-col justify-center items-center cursor-pointer transition-transform hover:scale-[1.1]"
-                    onClick={() => { applyFilter('categories', ['Headphones'])}}
+                    onClick={() => { filterAndNavigate('Headphones')}}
                 >
                     <img className="h-60" src={headphone} />
                     <span className="font-semibold text-lg">Headphones</span>
                 </div>
                 <div className="flex flex-col justify-center items-center cursor-pointer hover:scale-[1.1]"
-                    onClick={() => { applyFilter('categories', ['Earbuds'])}}
+                    onClick={() => { filterAndNavigate('Earbuds')}}
                 >
                     <img className="h-60" src={earbuds} />
                     <span className="font-semibold text-lg">Earbuds</span>
                 </div>
                 <div className="flex flex-col justify-center items-center cursor-pointer hover:scale-[1.1]"
-                    onClick={() => { applyFilter('categories', ['Speakers'])}}
+                    onClick={() => { filterAndNavigate('Speakers')}}
                 >
                     <img className="h-60" src={smwatch} />
                     <span className="font-semibold text-lg">Speakers</span>
